a29c97dc6c6d1e354a31747306018e5939b295a3
Sam Hastings <samhastingsis@gmail.com>
2023-07-08 12:37:48 -0400

    Initial Commit

---
+++ Squat Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Squat Counter
+//
+//  Created by sam hastings on 08/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Squat CounterUITests/Squat_CounterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Squat_CounterUITestsLaunchTests.swift
+//  Squat CounterUITests
+//
+//  Created by sam hastings on 08/07/2023.
+//
+
+import XCTest
+
+final class Squat_CounterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
